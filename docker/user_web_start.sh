73aef1bde13aad3c0ab329f5121688a472a9ed3c
LuoJu <luoju@coding.net>
2020-01-12 23:06:04 +0800

    更新启动脚本update

--- docker/user_web_start.sh
+++ docker/user_web_start.sh
@@ -2,7 +2,7 @@ cd ../
 mvn clean package
 echo -e "package finish..."
 cd blog-user-web
-docker build -t my-user-web:1.0 .
+docker build -t user-web:1.0 .
 
 #启动容器
-#docker run --name my-user-web -d  --network  my-net user-web
+#docker run --name my-user-web -d  --network  my-net user-web:1.0
