9655a1bbb5b9d03b9a19c23608329407d75a69d6
LuoJu <luoju@coding.net>
2020-02-26 09:20:59 +0800

    反转32位数字

--- blog-user-web/src/main/java/com/hanslaser/blog/leetCode/颠倒二进制数字.java
+++ blog-user-web/src/main/java/com/hanslaser/blog/leetCode/颠倒二进制数字.java
@@ -9,10 +9,33 @@ public class 颠倒二进制数字 {
 
         while (n > 10) {
             int last = n % 10;
-            rev = last*10+last;
+            rev = last * 10 + last;
         }
 
 
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(reverseBits2(123));
+    }
+
+
+    public static int reverseBits2(int n) {
+
+        int i = 0;
+        while (n != 0) {
+            if (i >= Integer.MAX_VALUE / 10 || i <= Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            int last = n % 10;
+
+            i = i * 10 + last;
+
+            n = n / 10;
+        }
 
         return 0;
     }
